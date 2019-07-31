fa3105d66915cd9046a6e9de004875117ca39411
Daniel LORENZI <daniel.lorenzi@eic.fr>
2019-07-31 12:05:23 +0200

    corrections trajets
    qq corrections

--- conception/cestQuoiQueComment/trajet.py
+++ conception/cestQuoiQueComment/trajet.py
@@ -5,9 +5,19 @@ class trajet:
     km=120 #km allé retour
     cout=4 #comme ça, c'est tout
 
+    """ctor"""
     def __init__(self, contexteSocioEconomique):
         self.contexteSocioEconomique = contexteSocioEconomique
         cout = coutParDefaut()
 
+    """ Combien tu paye pour être transporté sur ce voyage."""
     def coutParDefaut(self):
-        return self.contexteSocioEconomique.prixAuKm * self.contexteSocioEconomique.participationPourUnVoyage
+        return self.prixDuVoyage() * self.contexteSocioEconomique.participationPourUnVoyage
+
+    """ Combien ça coute en vrai, un voyage, dans notre contexte socio economique."""
+    def prixDuVoyage(self):
+        return self.contexteSocioEconomique.prixAuKm * self.km
+
+    """ En fait on a décidé des sous que doit donner un passager pour un voyage."""
+    def redefinirLaParticipation(self, cout):
+        self.participationPourUnVoyage = self.prixDuVoyage() / cout
