dcf44cb56a3cbe2a0a505cd3fa80bcfc275d2fe9
Luc CHANY <lchany@groupeisagri.com>
2019-10-16 15:28:49 +0200

    renommages

--- karpoule/karpoule.py
+++ karpoule/karpoule.py
@@ -8,20 +8,18 @@ app = Flask(__name__)
 @app.route('/demo/', methods=['GET'])
 def reponse():
     nom = request.args.get("nom", None)
-    print("reçu le nom: {}".format(nom))
-
     reponse = {}
     if not nom:
         reponse["ERROR"] = "pas de nom fourni, svp fournissez un nom."
     else:
-        reponse["MESSAGE"] = "Salut {}. Pas grand chose ici pour l'instant.".format(nom)
+        reponse["MESSAGE"] = "Bonjour {}. Pas grand chose ici pour l'instant.".format(nom)
 
     return jsonify(reponse)
 
 
 @app.route('/')
 def index():
-    return "<h1>Y a quelqu'un ?</h1>"
+    return "<h1>Bienvenue dans Karpoule ! </h1>"
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
